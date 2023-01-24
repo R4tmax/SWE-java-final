69c93ec1314b0be082305f33a497efa08fd933f1
R4tmax <kadlec.m.90@gmail.com>
2023-01-24 22:29:26 +0100

    MILESTONE1_BE - Rebased initial DB interaction implementation onto improved FE

--- src/main/java/cz/vse/nulltracker/nulltracker/core/LoginController.java
+++ src/main/java/cz/vse/nulltracker/nulltracker/core/LoginController.java
@@ -1,5 +1,11 @@
 package cz.vse.nulltracker.nulltracker.core;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -7,6 +13,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.Objects;
 
 public class LoginController {
     public Button submitButton;
@@ -18,4 +28,34 @@ public class LoginController {
         Main main = (Main) stage.getUserData();
         main.navigateTo("registration");
     }
+
+    public void attemptLogin() {
+
+        String login = emailInput.getText();
+        String pass = passwordInput.getText();
+
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://martin_dev:wahB7g4jjP2CCJ7@nulltrackerdev.nxwgnwc.mongodb.net/?retryWrites=true&w=majority");
+        MongoDatabase database;
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            database = mongoClient.getDatabase("NullTracerkerDevDB");
+            MongoCollection<Document> collection = database.getCollection("users");
+
+            Bson filter = Filters.regex("login", login);
+
+            Document entry = collection.find(filter).first();
+
+            if (entry == null) {
+                System.out.println("Incorrect login");
+            } else {
+                if(!Objects.equals(entry.getString("password"), pass)) {
+                    System.out.println("Incorrect password");
+                } else System.out.println("Login successful!");
+            }
+
+        }
+
+    }
+
+
 }
--- src/main/java/cz/vse/nulltracker/nulltracker/core/RegistrationController.java
+++ src/main/java/cz/vse/nulltracker/nulltracker/core/RegistrationController.java
@@ -1,10 +1,21 @@
 package cz.vse.nulltracker.nulltracker.core;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import java.util.Objects;
 
 public class RegistrationController {
     public Button submitButton;
@@ -18,4 +29,46 @@ public class RegistrationController {
         Main main = (Main) stage.getUserData();
         main.navigateTo("login");
     }
+
+    public void attemptRegistration() {
+
+        String name = nameInput.getText();
+        String login = emailInput.getText();
+        String pass = passwordFirstInput.getText();
+        String secondPass = passwordSecondInput.getText();
+
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://martin_dev:wahB7g4jjP2CCJ7@nulltrackerdev.nxwgnwc.mongodb.net/?retryWrites=true&w=majority");
+        MongoDatabase database;
+
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            database = mongoClient.getDatabase("NullTracerkerDevDB");
+            MongoCollection<Document> collection = database.getCollection("users");
+
+            Bson filter = Filters.regex("login", login);
+
+            Document entry = collection.find(filter).first();
+
+            if (entry != null) {
+                System.out.println("User already exists");
+                return;
+            }
+
+            if (!Objects.equals(pass, secondPass)) {
+                System.out.println("Passwords do not match");
+                return;
+            }
+
+
+            Document document = new Document("login", login)
+                    .append("password", pass).append("name", name);
+
+            collection.insertOne(document);
+            ObjectId objectId = document.getObjectId("_id");
+
+            System.out.println("Created user:" + objectId);
+
+        }
+    }
+
+
 }
