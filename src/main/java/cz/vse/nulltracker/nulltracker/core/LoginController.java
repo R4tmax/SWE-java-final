dd541b5947dd9b5d96609c375aed22e7c8ab01e8
Martin Kadlec2 <martin.kadlec2@lundegaard.eu>
2023-01-25 09:07:15 +0100

    MILESTONE1_BE - simplify login DB call using preopened connection and static API key refference, optimize imports

--- src/main/java/cz/vse/nulltracker/nulltracker/core/LoginController.java
+++ src/main/java/cz/vse/nulltracker/nulltracker/core/LoginController.java
@@ -1,14 +1,8 @@
 package cz.vse.nulltracker.nulltracker.core;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -17,6 +11,8 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import java.util.Objects;
+
+import static cz.vse.nulltracker.nulltracker.database.DatabaseHandler.database;
 
 public class LoginController {
     public Button submitButton;
@@ -40,11 +36,8 @@ public class LoginController {
         String login = emailInput.getText();
         String pass = passwordInput.getText();
 
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://martin_dev:wahB7g4jjP2CCJ7@nulltrackerdev.nxwgnwc.mongodb.net/?retryWrites=true&w=majority");
-        MongoDatabase database;
 
-        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            database = mongoClient.getDatabase("NullTracerkerDevDB");
+        try  {
             MongoCollection<Document> collection = database.getCollection("users");
 
             Bson filter = Filters.regex("login", login);
@@ -62,6 +55,8 @@ public class LoginController {
                 }
             }
 
+        } catch (Exception e) {
+            System.out.println("DB error" + e);
         }
 
     }
