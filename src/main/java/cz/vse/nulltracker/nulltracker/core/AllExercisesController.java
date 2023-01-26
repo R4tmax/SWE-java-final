1affc45a43a59239003775ed71f2056790dc062a
Michal Průcha <michal.prucha80@gmail.com>
2023-01-26 17:00:57 +0100

    fxml connectors between view and controller

--- src/main/java/cz/vse/nulltracker/nulltracker/core/AllExercisesController.java
+++ src/main/java/cz/vse/nulltracker/nulltracker/core/AllExercisesController.java
@@ -1,9 +1,20 @@
 package cz.vse.nulltracker.nulltracker.core;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class AllExercisesController {
 
+    public VBox mainListOfExercises;
+    public HBox category;
+    public Text categoryName;
+    public ListView exercisesOfCategory;
+    public VBox descriptionRegion;
+    public Text descriptionTitle;
+    public Text descriptionText;
 }
