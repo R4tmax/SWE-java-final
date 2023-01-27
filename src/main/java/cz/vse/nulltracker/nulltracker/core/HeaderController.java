33602e2564aa1563329725676ec3be1e4e293344
R4tmax <kadlec.m.90@gmail.com>
2023-01-27 18:21:03 +0100

    MILESTONE1_Refactoring - Added a periodic cycle to track user name

--- src/main/java/cz/vse/nulltracker/nulltracker/core/HeaderController.java
+++ src/main/java/cz/vse/nulltracker/nulltracker/core/HeaderController.java
@@ -1,12 +1,17 @@
 package cz.vse.nulltracker.nulltracker.core;
 
 import cz.vse.nulltracker.nulltracker.database.LoggedUser;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class HeaderController {
     public Hyperlink allExercisesLink;
@@ -24,6 +29,18 @@ public class HeaderController {
     public void linkToHome(MouseEvent mouseEvent) {
         Main main = (Main) stage.getUserData();
         main.navigateTo("dashboard");
+    }
+
+
+    public void initialize() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(5), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                updateName();
+            }
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 
     public void updateName () {
