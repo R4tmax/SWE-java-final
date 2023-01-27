ef2dd92af5c4ab4c121d40415a34e1edb1679145
R4tmax <kadlec.m.90@gmail.com>
2023-01-27 22:58:22 +0100

    MILESTONE2_BE - Typo fix, load the contents of the JSON into the ComboBox

--- src/main/java/cz/vse/nulltracker/nulltracker/core/NewWorkoutController.java
+++ src/main/java/cz/vse/nulltracker/nulltracker/core/NewWorkoutController.java
@@ -1,14 +1,58 @@
 package cz.vse.nulltracker.nulltracker.core;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class NewWorkoutController {
-    public ComboBox activitiySelector;
+    public ComboBox<Exercise> activitySelector;
     public DatePicker timestampCalendar;
     public Label attribute1;
     public Label attribute2;
     public Label attribute3;
     public Label attribute4;
+
+    public void initialize () throws FileNotFoundException {
+
+        ArrayList<Exercise> allExercises = new ArrayList<>();
+        File input = new File("src/main/resources/cz/vse/nulltracker/nulltracker/core/exercises.json");
+        JsonElement fileElement = JsonParser.parseReader(new FileReader(input));
+        JsonObject fileObject = fileElement.getAsJsonObject();
+        JsonArray categories = fileObject.get("workouts").getAsJsonArray();
+
+        categories.forEach(category -> {
+            JsonObject categoryObject = category.getAsJsonObject();
+            categoryObject.get("exercises").getAsJsonArray().forEach(exercise -> {
+                JsonObject exerciseObject = exercise.getAsJsonObject();
+                String name = exerciseObject.get("name").getAsString();
+                String description = exerciseObject.get("description").getAsString();
+                allExercises.add(new Exercise(name, description, exerciseObject.get("parameters").getAsJsonObject().asMap()));
+            });
+        });
+
+        activitySelector.getItems().addAll(allExercises);
+        activitySelector.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(Exercise item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null || item.getName() == null) {
+                    setText(null);
+                } else {
+                    setText(item.getName());
+                }
+            }
+        });
+
+    }
+
 }
