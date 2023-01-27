4cce3dee657f5121187408641842abeb51c09870
R4tmax <kadlec.m.90@gmail.com>
2023-01-27 22:33:37 +0100

    MILESTONE2_BE - Prepare the FX IDs for further manipulation

--- src/main/java/cz/vse/nulltracker/nulltracker/core/NewWorkoutController.java
+++ src/main/java/cz/vse/nulltracker/nulltracker/core/NewWorkoutController.java
@@ -1,4 +1,14 @@
 package cz.vse.nulltracker.nulltracker.core;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
 public class NewWorkoutController {
+    public ComboBox activitiySelector;
+    public DatePicker timestampCalendar;
+    public Label attribute1;
+    public Label attribute2;
+    public Label attribute3;
+    public Label attribute4;
 }
