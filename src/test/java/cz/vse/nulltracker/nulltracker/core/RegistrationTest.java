870113dbcb5b3ac68cd23977f8178311efd60225
Maxmilián Staněk <stam25@vse.cz>
2023-01-29 17:33:20 +0100

    Test reg

--- src/test/java/cz/vse/nulltracker/nulltracker/core/RegistrationTest.java
+++ src/test/java/cz/vse/nulltracker/nulltracker/core/RegistrationTest.java
@@ -12,6 +12,7 @@ import static cz.vse.nulltracker.nulltracker.database.DatabaseHandler.database;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegistrationTest {
+    String mainErrorMessage = null; //can save error message here
 
 
     private Document registrationAbstraction (String name, String login, String pass, String secondPass) {
@@ -23,22 +24,26 @@ class RegistrationTest {
             Document entry = userCollection.find(filter).first();
 
             if (entry != null) {
-                System.out.println("Existujici zaznam");
+                System.out.println("Existujici záznam"); //probably not needed
+                mainErrorMessage = "Existujici záznam";
                 return null;
             }
 
             if (!login.matches("^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")) {
-                System.out.println("Chyba v loginu");
+                System.out.println("nesprávný formát emailu");
+                mainErrorMessage = "nesprávný formát emailu";
                 return null;
             }
 
             if (!Objects.equals(pass, secondPass)) {
-                System.out.println("hesla se neshoduji");
+                System.out.println("hesla se neshodují");
+                mainErrorMessage = "hesla se neshodují";
                 return null;
             }
 
             if (!isPassSafe(pass)) {
-                System.out.println("Slabe heslo");
+                System.out.println("Slabé heslo");
+                mainErrorMessage = "Slabé heslo";
                 return null;
             }
 
@@ -99,8 +104,8 @@ class RegistrationTest {
 
         //test set A
         //Correct format
-        name = "Tomio Okamura";
-        login = "iluvbabis@spd.cz";
+        name = "Bloody Mary";
+        login = "MaryBlod@blood.bl";
         pass = "morava123";
         secondPass = "morava123";
 
@@ -126,10 +131,10 @@ class RegistrationTest {
 
         //test set B
         //Correct format
-        name = "Jarda Bašta";
-        login = "iluvdemocracykekw@spd.cz";
-        pass = "morava123";
-        secondPass = "morava123";
+        name = "Ardnold schwarzenegger";
+        login = "schwarzenegger@comewithmeifyouwanttolive.cz";
+        pass = "terminatornumber1";
+        secondPass = "terminatornumber1";
 
         entry = registrationAbstraction(name,login,pass,secondPass);
 
@@ -165,7 +170,103 @@ class RegistrationTest {
             userCollection.deleteOne(Filters.eq("_id", entry.getObjectId("_id")));
         }
         assertFalse(exists);
+        assertEquals("Existujici záznam", mainErrorMessage);
 
     }
+
+    @Test
+    public void incorrectInputWrongEmailFormat () {
+
+        boolean exists = false;
+        MongoCollection<Document> userCollection = database.getCollection("users");
+        Document entry;
+
+        String name;
+        String login;
+        String pass;
+        String secondPass;
+
+
+        //test set C
+        //Correct format
+        name = "James Bond";
+        login = "MyNamesIsBond";
+        pass = "5555";
+        secondPass = "5555";
+
+        entry = registrationAbstraction(name,login,pass,secondPass);
+        if (entry != null) {
+            exists = true;
+            userCollection.deleteOne(Filters.eq("_id", entry.getObjectId("_id")));
+        }
+        assertFalse(exists);
+        assertEquals("nesprávný formát emailu", mainErrorMessage);
+
+    }
+
+
+    @Test
+    public void incorrectInputErorrMatchingPassword () {
+
+        boolean exists = false;
+        MongoCollection<Document> userCollection = database.getCollection("users");
+        Document entry;
+
+        String name;
+        String login;
+        String pass;
+        String secondPass;
+
+
+        //test set C
+        //Correct format
+        name = "Jake Sully";
+        login = "sully@Pandora.galaxy";
+        pass = "smurf123";
+        secondPass = "5555";
+
+        entry = registrationAbstraction(name,login,pass,secondPass);
+        if (entry != null) {
+            exists = true;
+            userCollection.deleteOne(Filters.eq("_id", entry.getObjectId("_id")));
+        }
+        assertFalse(exists);
+        assertEquals("hesla se neshodují", mainErrorMessage);
+
+    }
+
+
+    @Test
+    public void incorrectInputWeakPassword () {
+
+        boolean exists = false;
+        MongoCollection<Document> userCollection = database.getCollection("users");
+        Document entry;
+
+        String name;
+        String login;
+        String pass;
+        String secondPass;
+
+
+        //test set C
+        //Correct format
+        name = "Puss in Boots";
+        login = "whereisShrek6@shrek.com";
+        pass = "whatareyoudoinginmyswamp";
+        secondPass = "whatareyoudoinginmyswamp";
+
+        entry = registrationAbstraction(name,login,pass,secondPass);
+        if (entry != null) {
+            exists = true;
+            userCollection.deleteOne(Filters.eq("_id", entry.getObjectId("_id")));
+        }
+        assertFalse(exists);
+        assertEquals("Slabé heslo", mainErrorMessage);
+
+    }
+
+
+
 
 }
